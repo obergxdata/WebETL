34c36a9dfea4807d81d6cd1899d10163cd9d856c
obergxdata <obergxdata@gmail.com>
2026-01-10 12:26:45 +0100

    Better request header

--- extract/http.py
+++ extract/http.py
@@ -5,11 +5,25 @@ logger = logging.getLogger(__name__)
 
 
 def visit_html(url, text=True):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.9",
+        "Accept-Encoding": "gzip, deflate, br",
+        "DNT": "1",
+        "Connection": "keep-alive",
+        "Upgrade-Insecure-Requests": "1",
+        "Sec-Fetch-Dest": "document",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-Site": "none",
+        "Cache-Control": "max-age=0",
+    }
+
     try:
         response = requests.get(
             url,
             timeout=10,
-            headers={"User-Agent": "RSSIFY/1.0"},
+            headers=headers,
         )
         response.raise_for_status()
         if text:
